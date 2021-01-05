f683496ae03d0ebb3464a1f1e2c790af97597e5a
admin <admin@DESKTOP-9K62JTN>
2021-01-05 18:04:27 +0100

    modification des noms

--- src/main/java/com/springboot/service/DeliberationTest.java
+++ src/main/java/com/springboot/service/DeliberationTest.java
@@ -1,205 +1,205 @@
-package com.springboot.service;
-
-import java.io.ByteArrayInputStream;
-import java.io.File;  
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
-import org.apache.commons.io.FileUtils;
-import org.apache.poi.hssf.usermodel.HSSFSheet;  
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;  
-import org.apache.poi.ss.usermodel.Cell;  
-import org.apache.poi.ss.usermodel.FormulaEvaluator;  
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import com.springboot.dao.EtudiantRepository;
-import com.springboot.entities.Etudiant;  
-
-public class DeliberationTest {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//ScriptEngineManagerUtils.listEngines();
-
-		//@Autowired
-		EtudiantRepository er;
-
-		List<Etudiant> listetd = new ArrayList<Etudiant>();
-		List<String> columns= new ArrayList<String>();
-		//obtaining input bytes from a file  
-		//File dude = new File();
-		//FileInputStream fip = new FileInputStream(file)
-		
-		
-		byte[] mybytes=null;
-		try {
-			mybytes = FileUtils.readFileToByteArray(new File("D:\\Bachelor\\S5\\JEE\\Gestion Deliberations\\test.xlsx"));
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		
-		
-		
-		InputStream fip = new ByteArrayInputStream(mybytes);  
-		//creating workbook instance that refers to .xls file  
-		XSSFWorkbook wb = null;
-		try {
-			wb = new XSSFWorkbook(fip);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}   
-		//creating a Sheet object to retrieve the object  
-		XSSFSheet sheet=wb.getSheetAt(0);  
-		//evaluating cell type   
-		int i=0;
-		for(Row row: sheet)     //iteration over row using for each loop  
-		{  
-			Etudiant e = new Etudiant();
-			int j=0;
-			int k=0;
-			while(row.getCell(k)!= null) {
-				//System.out.println("row:"+k+": "+row.getCell(k));
-				if(i==0) {
-					columns.add(""+row.getCell(k));
-				}
-				else{
-					String str= ""+row.getCell(k);
-					switch(k) {
-					case 0:
-						e.setCne(str);
-						System.out.println("massstr:"+str+" k:"+k);
-						break;
-					case 1:
-						e.setNom_etud(str);;
-						System.out.println("str:"+str);
-						break;
-					case 2:
-						e.setPrenom_etud(str);
-						System.out.println("str:"+str);
-						break;
-
-					case 3:
-						e.setTel_etud(String.valueOf((int) Double.parseDouble(str)));
-						System.out.println("str:"+str);
-						break;
-					case 4:
-						e.setEmail_etud(str);
-						System.out.println("str:"+str);
-						break;
-					case 5:
-						//if(str.contains("etape")) e.setEtape(etape);
-
-						e.setAnnee_academique(str);
-
-						System.out.println("str:"+str);
-						break;
-					case 6:
-						//if(str.contains("filiere")) e.setFiliere(filiere);
-						break;
-					case 7:
-						//if(str.contains("nom")) e.setNom_etud(str);;
-						break;
-					case 8:
-						//if(str.contains("nom")) e.setNom_etud(str);;
-						break;
-
-					}
-				}
-				k++;
-			}
-			i++;
-			/*for(Cell cell: row)    //iteration over cell using for each loop  
-			{  
-				if(i!=0)
-					switch(formulaEvaluator.evaluateInCell(cell).getCellType())  
-					{  
-					case NUMERIC:   //field that represents numeric cell type  
-						//getting the value of the cell as a number  
-						System.out.print(cell.getNumericCellValue()+ "\t\t"); 
-						switch(j) {
-						case 5:
-							Date d = new Date();
-							d.setYear((int) cell.getNumericCellValue());
-							e.setAnnee_academique(d);
-							j++;
-							break;
-						case 6:
-							Etape p = new Etape();
-							p.setId_etape((long)cell.getNumericCellValue());
-							e.setEtape(p);
-							j++;
-							break;
-						case 8:
-							InscriptionPedagogique in = new InscriptionPedagogique();
-							in.setId_inscription_pedagogique((long) cell.getNumericCellValue());
-							//e.setInscriptionPedagogiques(in);
-							j++;
-							break;
-						}
-						break;  
-					case STRING:    //field that represents string cell type  
-						//getting the value of the cell as a string  
-						System.out.print(cell.getStringCellValue()+ "\t\t");
-						switch(j) {
-						case 0:
-							e.setMassarEtud(cell.getStringCellValue());
-							j++;
-							break;
-						case 1:
-							e.setNom_etud(cell.getStringCellValue());
-							j++;
-							break;
-						case 2:
-							e.setPrenom_etud(cell.getStringCellValue());
-							j++;
-							break;
-						case 3:
-							e.setTel_etud(cell.getStringCellValue());
-							j++;
-							break;
-						case 4:
-							e.setEmail_etud(cell.getStringCellValue());
-							j++;
-							break;
-						case 7:
-
-						//	e.setFiliere(new Filiere().setNom_filiere(cell.getStringCellValue()));
-							j++;
-							break;
-						}
-						break;  
-					}  
-			}  */
-
-			listetd.add(e);
-			//er.save(e);
-		}  
-
-		System.out.println("nom: "+listetd.get(1).getNom_etud());
-		System.out.println("prenom: "+listetd.get(1).getPrenom_etud());
-		System.out.println("teletud: "+listetd.get(1).getTel_etud());
-		System.out.println("mail: "+listetd.get(1).getEmail_etud());
-		//System.out.println("annne acc: "+listetd.get(1).annee_academique);
-		//System.out.println("etape: "+listetd.get(0).getEtape().getId_etape());
-		//System.out.println("filier: "+listetd.get(1).getFiliere().getId_filiere());
-		//System.out.println("insc pedag: "+listetd.get(0).getMassarEtud());
-		System.out.println("chop");/*
-		for(int v=0;v<columns.size();v++) {
-			System.out.println("columns["+v+"]: "+columns.get(v));
-
-		}*/
-
-	}
-	
-
-}
+//package com.springboot.service;
+//
+//import java.io.ByteArrayInputStream;
+//import java.io.File;  
+//import java.io.FileInputStream;
+//import java.io.FileNotFoundException;
+//import java.io.IOException;
+//import java.io.InputStream;
+//import java.util.ArrayList;
+//import java.util.Date;
+//import java.util.List;
+//
+//import org.apache.commons.io.FileUtils;
+//import org.apache.poi.hssf.usermodel.HSSFSheet;  
+//import org.apache.poi.hssf.usermodel.HSSFWorkbook;  
+//import org.apache.poi.ss.usermodel.Cell;  
+//import org.apache.poi.ss.usermodel.FormulaEvaluator;  
+//import org.apache.poi.ss.usermodel.Row;
+//import org.apache.poi.xssf.usermodel.XSSFSheet;
+//import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+//import org.springframework.beans.factory.annotation.Autowired;
+//
+//import com.springboot.dao.EtudiantRepository;
+//import com.springboot.entities.Etudiant;  
+//
+//public class DeliberationTest {
+//
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		//ScriptEngineManagerUtils.listEngines();
+//
+//		//@Autowired
+//		EtudiantRepository er;
+//
+//		List<Etudiant> listetd = new ArrayList<Etudiant>();
+//		List<String> columns= new ArrayList<String>();
+//		//obtaining input bytes from a file  
+//		//File dude = new File();
+//		//FileInputStream fip = new FileInputStream(file)
+//		
+//		
+//		byte[] mybytes=null;
+//		try {
+//			mybytes = FileUtils.readFileToByteArray(new File("D:\\Bachelor\\S5\\JEE\\Gestion Deliberations\\test.xlsx"));
+//		} catch (IOException e1) {
+//			// TODO Auto-generated catch block
+//			e1.printStackTrace();
+//		}
+//		
+//		
+//		
+//		InputStream fip = new ByteArrayInputStream(mybytes);  
+//		//creating workbook instance that refers to .xls file  
+//		XSSFWorkbook wb = null;
+//		try {
+//			wb = new XSSFWorkbook(fip);
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}   
+//		//creating a Sheet object to retrieve the object  
+//		XSSFSheet sheet=wb.getSheetAt(0);  
+//		//evaluating cell type   
+//		int i=0;
+//		for(Row row: sheet)     //iteration over row using for each loop  
+//		{  
+//			Etudiant e = new Etudiant();
+//			int j=0;
+//			int k=0;
+//			while(row.getCell(k)!= null) {
+//				//System.out.println("row:"+k+": "+row.getCell(k));
+//				if(i==0) {
+//					columns.add(""+row.getCell(k));
+//				}
+//				else{
+//					String str= ""+row.getCell(k);
+//					switch(k) {
+//					case 0:
+//						e.setCne(str);
+//						System.out.println("massstr:"+str+" k:"+k);
+//						break;
+//					case 1:
+//						e.setNom_etud(str);;
+//						System.out.println("str:"+str);
+//						break;
+//					case 2:
+//						e.setPrenom_etud(str);
+//						System.out.println("str:"+str);
+//						break;
+//
+//					case 3:
+//						e.setTel_etud(String.valueOf((int) Double.parseDouble(str)));
+//						System.out.println("str:"+str);
+//						break;
+//					case 4:
+//						e.setEmail_etud(str);
+//						System.out.println("str:"+str);
+//						break;
+//					case 5:
+//						//if(str.contains("etape")) e.setEtape(etape);
+//
+//						e.setAnnee_academique(str);
+//
+//						System.out.println("str:"+str);
+//						break;
+//					case 6:
+//						//if(str.contains("filiere")) e.setFiliere(filiere);
+//						break;
+//					case 7:
+//						//if(str.contains("nom")) e.setNom_etud(str);;
+//						break;
+//					case 8:
+//						//if(str.contains("nom")) e.setNom_etud(str);;
+//						break;
+//
+//					}
+//				}
+//				k++;
+//			}
+//			i++;
+//			/*for(Cell cell: row)    //iteration over cell using for each loop  
+//			{  
+//				if(i!=0)
+//					switch(formulaEvaluator.evaluateInCell(cell).getCellType())  
+//					{  
+//					case NUMERIC:   //field that represents numeric cell type  
+//						//getting the value of the cell as a number  
+//						System.out.print(cell.getNumericCellValue()+ "\t\t"); 
+//						switch(j) {
+//						case 5:
+//							Date d = new Date();
+//							d.setYear((int) cell.getNumericCellValue());
+//							e.setAnnee_academique(d);
+//							j++;
+//							break;
+//						case 6:
+//							Etape p = new Etape();
+//							p.setId_etape((long)cell.getNumericCellValue());
+//							e.setEtape(p);
+//							j++;
+//							break;
+//						case 8:
+//							InscriptionPedagogique in = new InscriptionPedagogique();
+//							in.setId_inscription_pedagogique((long) cell.getNumericCellValue());
+//							//e.setInscriptionPedagogiques(in);
+//							j++;
+//							break;
+//						}
+//						break;  
+//					case STRING:    //field that represents string cell type  
+//						//getting the value of the cell as a string  
+//						System.out.print(cell.getStringCellValue()+ "\t\t");
+//						switch(j) {
+//						case 0:
+//							e.setMassarEtud(cell.getStringCellValue());
+//							j++;
+//							break;
+//						case 1:
+//							e.setNom_etud(cell.getStringCellValue());
+//							j++;
+//							break;
+//						case 2:
+//							e.setPrenom_etud(cell.getStringCellValue());
+//							j++;
+//							break;
+//						case 3:
+//							e.setTel_etud(cell.getStringCellValue());
+//							j++;
+//							break;
+//						case 4:
+//							e.setEmail_etud(cell.getStringCellValue());
+//							j++;
+//							break;
+//						case 7:
+//
+//						//	e.setFiliere(new Filiere().setNom_filiere(cell.getStringCellValue()));
+//							j++;
+//							break;
+//						}
+//						break;  
+//					}  
+//			}  */
+//
+//			listetd.add(e);
+//			//er.save(e);
+//		}  
+//
+//		System.out.println("nom: "+listetd.get(1).getNom_etud());
+//		System.out.println("prenom: "+listetd.get(1).getPrenom_etud());
+//		System.out.println("teletud: "+listetd.get(1).getTel_etud());
+//		System.out.println("mail: "+listetd.get(1).getEmail_etud());
+//		//System.out.println("annne acc: "+listetd.get(1).annee_academique);
+//		//System.out.println("etape: "+listetd.get(0).getEtape().getId_etape());
+//		//System.out.println("filier: "+listetd.get(1).getFiliere().getId_filiere());
+//		//System.out.println("insc pedag: "+listetd.get(0).getMassarEtud());
+//		System.out.println("chop");/*
+//		for(int v=0;v<columns.size();v++) {
+//			System.out.println("columns["+v+"]: "+columns.get(v));
+//
+//		}*/
+//
+//	}
+//	
+//
+//}
--- src/main/java/com/springboot/web/AdminiController.java
+++ src/main/java/com/springboot/web/AdminiController.java
@@ -4,15 +4,21 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import com.springboot.dao.AnneeUniversitaireRepository;
 import com.springboot.dao.EtudiantRepository;
 import com.springboot.dao.FiliereRepository;
 import com.springboot.dao.InscriptionAdministrativeRepository;
 import com.springboot.dao.InscriptionEnligneRepository;
+import com.springboot.dao.InscriptionPedagogiqueRepository;
+import com.springboot.dao.ModuleRepository;
+import com.springboot.entities.AnneeUniversitaire;
+import com.springboot.entities.Etablissement;
 import com.springboot.entities.Etape;
 import com.springboot.entities.Etudiant;
 import com.springboot.entities.Filiere;
 import com.springboot.entities.InscriptionAdministrative;
 import com.springboot.entities.InscriptionEnligne;
+import com.springboot.entities.InscriptionPedagogique;
 import com.springboot.entities.Module;
 import com.springboot.entities.Semestre;
 
@@ -22,6 +28,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -45,151 +52,69 @@ public class AdminiController {
 	@Autowired
 	com.springboot.dao.SemestreRepository semestreRepository;
 
+	@Autowired
+	com.springboot.dao.InscriptionPedagogiqueRepository inscriptionPedagogiqueRepository;
+	@Autowired
+	AnneeUniversitaireRepository anneeUniversitaireRepository;
 
+	//2.Inscription Administrative
 
-	//1.Inscription Enligne
-
-
-	//Affichage avec pagination : chercher
-	@GetMapping(path="/enlignes") 
-	public String listEnligne(Model model ,
-			@RequestParam(name="page",defaultValue = "0")int page ,
-			@RequestParam(name="size",defaultValue = "5")int size , 
-			@RequestParam(name="keyword",defaultValue = "")String keyword) {
-		Page<InscriptionEnligne> pageEnlignes = inscriptionEnligneRepository.findByNom_frContains(keyword,PageRequest.of(page, size));
-		model.addAttribute("enlignes",pageEnlignes.getContent());
-		model.addAttribute("pages",new int[pageEnlignes.getTotalPages()]);
-		model.addAttribute("currentPage",page);
-		model.addAttribute("keyword",keyword);
-		model.addAttribute("size",size);
-
-		return "listeEnligne";
-	}
-
-	//Affichage avec pagination :tous
-	@GetMapping(path="/enlignesAll") 
-	public String listEnligneAll(Model model ,
-			@RequestParam(name="page",defaultValue = "0")int page ,
-			@RequestParam(name="size",defaultValue = "5")int size , 
-			@RequestParam(name="keyword",defaultValue = "")String keyword) {
-		Page<InscriptionEnligne> pageEnlignes = inscriptionEnligneRepository.findAll(PageRequest.of(page, size));
-		model.addAttribute("enlignes",pageEnlignes.getContent());
-		model.addAttribute("pages",new int[pageEnlignes.getTotalPages()]);
-		model.addAttribute("currentPage",page);
-		model.addAttribute("keyword",keyword);
-		model.addAttribute("size",size);
-		return "listeEnligne";
-	}
-
-	//Insertion
-	@RequestMapping(path="/enligne")
-	public String inscriptionEnligne(Model model) {
-		model.addAttribute("enligne", new InscriptionEnligne());
-		model.addAttribute("mode", "new");
-		return "formEnligne";
-	}
-	//Supprission
-	@GetMapping(path="/deleteEnligne")
-	public String deleteEnligne(String id ) {
-		inscriptionEnligneRepository.deleteByCne(id);
-		return "redirect:/enlignesAll";
-	}
-	//Validation 
-	@RequestMapping(path="/saveEnligne" , method = RequestMethod.POST)
-	public String saveEnligne(Model model,@Valid InscriptionEnligne inscriptionEnligne,BindingResult bindingResult){
-		if(bindingResult.hasErrors()) return "formEnligne";
-		inscriptionEnligneRepository.save(inscriptionEnligne);
-		model.addAttribute("enligne", inscriptionEnligne);
-		return "EnregistrementEnligne";
-	}	 
-
-	//Valider InscriptionEnligne : mis valide_enligne=true
-	@GetMapping(path="/validerEnligne")
-	public String validerEnligne(Model model,String id ,
-			@RequestParam(value="bar", required = true , defaultValue = "true")
-	boolean bar) {
-		InscriptionEnligne enligne = inscriptionEnligneRepository.findByCne(id);
-		enligne.setValide_enligne(bar);
-		inscriptionEnligneRepository.save(enligne);
-		model.addAttribute("bar", bar);
-		model.addAttribute("enligne",enligne);
-		return "redirect:/enlignesAll?bar="+bar;
-	}
-	//Les enregistrements
-	@GetMapping(path="/ConfirmationEnligne")
-	public String ConfirmationEnligne(Model model,String id) {
-		InscriptionEnligne enligne = inscriptionEnligneRepository.findByCne(id);
-		model.addAttribute("enligne", enligne);
-		return "ConfirmationEnligne";
-	}
-
-
+	
 	@GetMapping(path="/ConfermationAdmin")
-	public String ConfermationAdmin(Model model , String id 
-			//@RequestParam(name = "id_filiere" , defaultValue = "1")Long id_filiere
+	public String ConfermationAdmin(Model model , 
+			@RequestParam(name = "id_enligne" , defaultValue = "")Long id_enligne, 
+			@RequestParam(name = "id_filiere" , defaultValue = "")String id_filiere,
+			@RequestParam(name = "annee" , defaultValue = "")String annee,
+			@RequestParam(name = "etablissement" , defaultValue = "")String etablissement
 			) {
-		InscriptionEnligne enligne = inscriptionEnligneRepository.getOne(id);
+		InscriptionEnligne enligne = inscriptionEnligneRepository.getOne(id_enligne);
 		model.addAttribute("enligne", enligne);
 		InscriptionAdministrative admin = new InscriptionAdministrative();
 		model.addAttribute("admin", admin);
-		id = inscriptionEnligneRepository.findByCne(id).getCne();
-		//Filiere filiere = filiereRepository.getOne(id_filiere);
-		//filiere.getNom_filiere();
-		//model.addAttribute("filiere", filiere);
-		model.addAttribute("cne", id);
-
+		id_enligne = inscriptionEnligneRepository.findById(id_enligne).get().getId();
+		List<Filiere> f = filiereRepository.findAll();
+		List<AnneeUniversitaire> u = anneeUniversitaireRepository.findAll();
+		
+		model.addAttribute("filiere", f);
+		model.addAttribute("enligne", id_enligne);
+		model.addAttribute("annees", u);
+		model.addAttribute("etablissement", etablissement);
+		
 
 		model.addAttribute("mode", "new");
 
 		return "ConfirmationAdmin";
 	}
-
-	@RequestMapping(path="/saveAdmin" , method = RequestMethod.POST)
-	public String saveAdmin(Model model ,@RequestParam("cne")String cne,@RequestParam("filiere")Long filiere,
-			@Valid InscriptionAdministrative admin,
-			BindingResult bindingResult){
-		if(bindingResult.hasErrors()) return "formAdmin";
-
-		InscriptionEnligne enligne = inscriptionEnligneRepository.findByCne(cne);
-		enligne.setCne(cne);
-		admin.setInscriptionEnligne(enligne);
-
-		Filiere f = filiereRepository.findById(filiere).get();
-		f.setId_filiere(filiere);
-		admin.setFiliere(f);
-
-		inscriptionAdministrativeRepository.save(admin);
-		//			
-		//			Etudiant e = new Etudiant();  
-		//			e.setNom_etud(enligne.getNom_fr());
-		//			e.setPrenom_etud(enligne.getPrenom_fr());
-		//			
-		//			e.setAnnee_academique(admin.getAnnee_academique());
-		//			
-		//			e.setEmail_etud(enligne.getEmail());
-		//			
-		//			e.setFiliere(admin.getFiliere());
-		//			
-		//			//e.setResultat(enligne.getResultat());
-		//			
-		//			e.setTel_etud(admin.getTelephone());
-		//			
-		//			e.setCne(enligne.getCne());
-		//			
-		//			etudiantRepository.save(e);
-
-		model.addAttribute("admin", admin);
-		model.addAttribute("enligne", enligne);
-		model.addAttribute("filiere", f);
-
-		return "redirect:/adminsAll";
-	}
+//
+//	@RequestMapping(path="/saveAdmin" , method = RequestMethod.POST)
+//	public String saveAdmin(Model model ,@RequestParam("cne")String cne,@RequestParam("filiere")Long filiere,
+//			@Valid InscriptionAdministrative admin,
+//			BindingResult bindingResult){
+//		if(bindingResult.hasErrors()) return "formAdmin";
+//
+//		InscriptionEnligne enligne = inscriptionEnligneRepository.findByCne(cne);
+//		enligne.setCne(cne);
+//		admin.setInscriptionEnligne(enligne);
+//
+//		Filiere f = filiereRepository.findById(filiere).get();
+//		f.setId(filiere);
+//		admin.setFiliere(f);
+//
+//		inscriptionAdministrativeRepository.save(admin);
+//
+//
+//		model.addAttribute("admin", admin);
+//		model.addAttribute("enligne", enligne);
+//		model.addAttribute("filiere", f);
+//
+//		return "redirect:/adminsAll";
+//	}
 	@GetMapping(path="/administrativeAll") 
 	public String listAdministrativeAll(Model model ,
 			@RequestParam(name="page",defaultValue = "0")int page ,
 			@RequestParam(name="size",defaultValue = "5")int size , 
 			@RequestParam(name="keyword",defaultValue = "")String keyword){
-		Page<InscriptionEnligne> pageEnlignes = inscriptionEnligneRepository.findByValide_enligne(PageRequest.of(page, size));
+		Page<InscriptionEnligne> pageEnlignes = inscriptionEnligneRepository.findByValideEnligne(PageRequest.of(page, size));
 
 		model.addAttribute("enligne",pageEnlignes.getContent());
 		model.addAttribute("pages",new int[pageEnlignes.getTotalPages()]);
@@ -206,7 +131,7 @@ public class AdminiController {
 			@RequestParam(name="page",defaultValue = "0")int page ,
 			@RequestParam(name="size",defaultValue = "5")int size , 
 			@RequestParam(name="keyword",defaultValue = "")String keyword) {
-		Page<InscriptionAdministrative> pageAdmins = inscriptionAdministrativeRepository.findByAnnee_academiqueContains(keyword, PageRequest.of(page, size));
+		Page<InscriptionAdministrative> pageAdmins = inscriptionAdministrativeRepository.findByNomContains(keyword, PageRequest.of(page, size));
 		model.addAttribute("admins",pageAdmins.getContent());
 		model.addAttribute("pages",new int[pageAdmins.getTotalPages()]);
 		model.addAttribute("currentPage",page);
@@ -232,31 +157,24 @@ public class AdminiController {
 	}
 	//validation Admini manuelle
 
-	@GetMapping(path="/validAdmin")
-	public String validAdmin(Model model,Long id ) {
-		InscriptionAdministrative admin = inscriptionAdministrativeRepository.findById(id).get();
-		InscriptionEnligne enligne = inscriptionEnligneRepository.findByCne(admin.getInscriptionEnligne().getCne());
-		Etudiant e = new Etudiant();  
-		e.setNom_etud(enligne.getNom_fr());
-		e.setPrenom_etud(enligne.getPrenom_fr());
-
-		e.setAnnee_academique(admin.getAnnee_academique());
-
-		e.setEmail_etud(enligne.getEmail());
-
-		e.setFiliere(admin.getFiliere());
-
-		//e.setResultat(enligne.getResultat());
-
-		e.setTel_etud(admin.getTelephone());
-
-		e.setCne(enligne.getCne());
-
-		etudiantRepository.save(e);
-
-		model.addAttribute("enligne",enligne);
-		return "redirect:/etudiantsAll";
-	}
+//	@GetMapping(path="/validAdmin")
+//	public String validAdmin(Model model,Long id ) {
+//		InscriptionAdministrative admin = inscriptionAdministrativeRepository.findById(id).get();
+//		InscriptionEnligne enligne = inscriptionEnligneRepository.findByCne(admin.getInscriptionEnligne().getCne());
+//		Etudiant e = new Etudiant();  
+//
+//		e.setCne(enligne.getCne());
+//		e.setNom(enligne.getNomFr());
+//		e.setPrenom(enligne.getPrenomFr());
+//
+//		e.setFiliere(admin.getFiliere());
+//		e.setTelephone(admin.getTelephone());
+//		e.setEmail(admin.getEmailEtud());
+//		etudiantRepository.save(e);
+//
+//		model.addAttribute("enligne",enligne);
+//		return "redirect:/etudiantsAll";
+//	}
 
 	//Supprission
 	@GetMapping(path="/deleteAdmin")
@@ -265,120 +183,5 @@ public class AdminiController {
 		return "redirect:/adminsAll";
 	}
 
-	//Supprission etape , semestre ,module
-	@GetMapping(path="/deleteEtape")
-	public String deleteEtape(Long id ) {
-		etapeRepository.deleteById(id);
-		return "redirect:/formEtape";
-	}
-	@GetMapping(path="/deleteSemestre")
-	public String deleteSemstre(Long id ) {
-		semestreRepository.deleteById(id);
-		return "redirect:/formEtape";
-	}
-	@GetMapping(path="/deleteModule")
-	public String deleteModule(Long id ) {
-		moduleRepository.deleteById(id);
-		return "redirect:/formEtape";
-	}
-
-	// 4.Inscription Pedagogique ,affecter filiere a l'etape
-
-	@GetMapping(path="/formEtape")
-	public String formEtape(Model model) {
-		Etape etape = new Etape();
-		model.addAttribute("etape", etape);
-		List<Etape> etapes = etapeRepository.findAll();
-		model.addAttribute("etapes", etapes);
-
-		Semestre semestre = new Semestre();
-		model.addAttribute("semestre", semestre);
-		List<Semestre> semestres = semestreRepository.findAll();
-		model.addAttribute("semestres", semestres);
-		Module module = new Module();
-		model.addAttribute("module", module);
-		List<Module> modules = moduleRepository.findAll();
-		model.addAttribute("modules", modules);
-
-		return "formEtape";
-	}
-	//Validation etape V2
-	@RequestMapping(path="/saveEtape" , method = RequestMethod.POST)
-	public String saveEtape(Model model,@Valid Etape  etape, @RequestParam("filiere")Long filiere,BindingResult bindingResult){
-		if(bindingResult.hasErrors()) return "formEtape";
-
-		Filiere f = filiereRepository.findById(filiere).get();
-		f.setId_filiere(filiere);
-		etape.setFiliere(f);
-
-		etapeRepository.save(etape);
-		model.addAttribute("etape", etape);
-		model.addAttribute("filiere", filiere);
-
-		return "redirect:/etapes";
-	}
-	@RequestMapping(path="/saveSemestre" , method = RequestMethod.POST) public
-	String saveEtape(Model model,@Valid Semestre
-			semestre,@RequestParam("id_etape")String id ,BindingResult bindingResult){
-		if(bindingResult.hasErrors()) return "formEtape";
-		
-		Long id_etape = etapeRepository.findId_etapeByLibelle_etape(id);
-		Etape etape = etapeRepository.findById(id_etape).get(); 
-		etape.setId_etape(id_etape);
-		semestre.setEtape(etape);
-
-		semestreRepository.save(semestre); 
-		model.addAttribute("semestre", semestre);
-
-		model.addAttribute("id_etape", id);
-
-		return "redirect:/etapes";
-	}
-	@RequestMapping(path="/saveModule" , method = RequestMethod.POST) public
-	String saveEtape(Model model,@Valid Module module
-			,@RequestParam("id_semestre")String id ,BindingResult bindingResult){
-		if(bindingResult.hasErrors()) return "formEtape";
-
-		Long id_semetre = semestreRepository.findId_moduleByLibelle_semestre(id);
-		Semestre semestre = semestreRepository.findById(id_semetre).get();
-		semestre.setId_semestre(id_semetre);
-		module.setSemestre(semestre);
-
-		moduleRepository.save(module);
-		model.addAttribute("module", module);
-
-		model.addAttribute("id_semestre", id);
-
-		return "redirect:/etapes";
-	}
-	@GetMapping(path="/etapes") 
-	public String listEtapes(Model model
-			) {
-		List<Etape> etapes = etapeRepository.findAll();
-		model.addAttribute("etapes", etapes);
-
-		List<Semestre> semestres = semestreRepository.findAll();
-		model.addAttribute("semestres", semestres);
-
-		List<Module> modules = moduleRepository.findAll();
-		model.addAttribute("modules", modules);
-
-		return "redirect:/formEtape";
-	}
-
-	/*
-	 * @GetMapping(path="/selectParFiliere") public String selectParFiliere(Model
-	 * model ,@RequestParam(name="filiere")Long id_filiere) { //List<Etudiant>
-	 * etudiants = etudiantRepository.findAllById_filiere(id_filiere); //Filiere
-	 * filiere = filiereRepository.findById_filiere(id_filiere);
-	 * 
-	 * Filiere f = filiereRepository.findById(id_filiere).get();
-	 * f.setId_filiere(id_filiere);
-	 * 
-	 * model.addAttribute("filiere", id_filiere);
-	 * 
-	 * model.addAttribute("etudiants",etudiants);
-	 * 
-	 * return "listeSemestre"; }
-	 */
 }
+
